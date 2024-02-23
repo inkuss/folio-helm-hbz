dc0b7c908ec9272a3407a4215f5ab22e983010f2
Florian Kreft <kreft@lrz.de>
2024-02-23 12:43:42 +0100

    fixed new user creation skript/job

--- tenant/files/createFolioUser.sh
+++ tenant/files/createFolioUser.sh
@@ -5,7 +5,9 @@ set -o pipefail
 # set this for debugging: VERBOSE="-v"
 VERBOSE=""
 
-echo "Creating user $USERNAME in Folio"
+OKAPICLI_TENANT="./OkapiCLI.py $VERBOSE --url $OKAPI_URL --username $FOLIO_ADMIN_USERNAME --password $FOLIO_ADMIN_PASSWORD --tenant $FOLIO_TENANTID raw"
+
+echo "Creating user $USERNAME in Folio with the following JSON"
 
 echo '{
   "username": "'$USERNAME'",
@@ -13,7 +15,7 @@ echo '{
   "type": "staff",
   "personal": {
     "lastName": "'$USERLASTNAME'",
-    "phone": "'$USERPHONE'"
+    "phone": "'$USERPHONE'",
     "email": "'$USEREMAIL'"
   }
 }' | ./OkapiCLI.py $VERBOSE --url $OKAPI_URL --username $FOLIO_ADMIN_USERNAME --password $FOLIO_ADMIN_PASSWORD --tenant $FOLIO_TENANTID raw --method post --rawpath /users
@@ -24,7 +26,7 @@ fi
 
 echo "Getting id of created user"
 
-NEW_USERID=`./OkapiCLI.py $VERBOSE --url $OKAPI_URL --username $FOLIO_ADMIN_USERNAME --password $FOLIO_ADMIN_PASSWORD --tenant $FOLIO_TENANTID raw --rawpath "/users?query=username=$NEW_USERNAME" |jq -r .users[0].id`
+NEW_USERID=`./OkapiCLI.py $VERBOSE --url $OKAPI_URL --username $FOLIO_ADMIN_USERNAME --password $FOLIO_ADMIN_PASSWORD --tenant $FOLIO_TENANTID raw --rawpath "/users?query=username=$USERNAME" |jq -r .users[0].id`
 
 if [ -z "$NEW_USERID" ] ; then
   echo "Error ferching new user id"
@@ -55,6 +57,19 @@ echo '{
 if [ "$?" != 0 ] ; then
   echo "Error setting permissions. Already set?"
 fi
+
+echo "Getting $FOLIO_ADMIN_USERNAME Permission Id"
+ADMIN_ID=$($OKAPICLI_TENANT --method get --rawpath "/users?query=(username==$FOLIO_ADMIN_USERNAME)" | jq -r '.users[0].id')
+ADMIN_PERMID=$($OKAPICLI_TENANT --method get --rawpath "/perms/users?query=(userId==$ADMIN_ID)" | jq -r '.permissionUsers | .[].id')
+
+echo "Getting Permissions Object for $FOLIO_ADMIN_USERNAME, user id: $ADMIN_ID, Permission Id: $ADMIN_PERMID"
+ADMIN_PERMOBJ=$($OKAPICLI_TENANT --method get --rawpath "/perms/users/$ADMIN_PERMID" |  jq -c )
+
+echo "Getting $USERNAME permission user id"
+NEW_PERMID=$($OKAPICLI_TENANT --method get --rawpath "/perms/users?query=(userId==$NEW_USERID)" | jq -r '.permissionUsers | .[].id'  )
+echo "Modifying Permission Object for $USERNAME, user id: $NEW_USERID, permission id: $NEW_PERMID"
+
+echo "$ADMIN_PERMOBJ" | jq --arg newid $NEW_PERMID --arg newuserid $NEW_USERID -r 'del(.metadata) | .id=$newid | .userId=$newuserid' | $OKAPICLI_TENANT -m put -r /perms/users/$NEW_PERMID
 
 echo "done"
 # Todo: check if all is set up correctly and exit 1 when not.
