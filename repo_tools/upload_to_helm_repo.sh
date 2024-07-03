e77314970c6564dc0942a52ca1a81e979bf2a36d
Florian Kreft <kreft@lrz.de>
2024-07-03 11:54:08 +0200

    added missing entry for lib-bvb-folio to upload helm repo Skript

--- repo_tools/upload_to_helm_repo.sh
+++ repo_tools/upload_to_helm_repo.sh
@@ -27,7 +27,7 @@ echo "Uploading"
 username=gitlab-ci-token
 password=$CI_JOB_TOKEN
 
-for i in backups*.tgz edge*.tgz modules*.tgz okapi*.tgz secure-supertenant*.tgz tenant*.tgz zalando-pg*.tgz vufind*.tgz zalando-vufind-pg*.tgz b3katimport*.tgz ; do
+for i in backups*.tgz edge*.tgz modules*.tgz okapi*.tgz secure-supertenant*.tgz tenant*.tgz zalando-pg*.tgz vufind*.tgz zalando-vufind-pg*.tgz b3katimport*.tgz lib-bvb-folio*.tgz; do
    curl --request POST --user "$username:$password" --form "chart=@$i" "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/packages/helm/api/${HELM_REPO_DST}/charts"
    echo
 done
